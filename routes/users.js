13b4577b82ea4fbcb4ad93ce004ef1a5c2783b23
Muljayan <37667437+Muljayan@users.noreply.github.com>
2018-05-02 18:41:09 +0530

    Registration form validation

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoose =require('mongoose');
-const Idea = mongoose.model('idea');
 
 //Login Route
 router.get('/login',(req,res)=>{
@@ -13,5 +12,27 @@ router.get('/login',(req,res)=>{
 router.get('/register',(req,res)=>{
     res.render('users/register')
 });
+
+//Register form POST
+router.post('/register', (req, res) => {
+    let errors = [];
+    if(req.body.password != req.body.password2){
+        errors.push({text:'Passwords do not match'});
+    }
+    if(req.body.password.length < 5){
+        errors.push({text:'Password must be atleast 5 characters long'})
+    }
+    if(errors.length>0){
+        res.render('users/register',{
+            errors:errors,
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password,
+            password2:req.body.password2
+        })
+    }else{
+        res.send('Passed')
+    }
+  });
 
 module.exports = router;
